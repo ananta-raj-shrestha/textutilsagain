2d99e6eaf11a85f2d501dfa52de19579d0a44212
eranantarajshrestha <eranantarajshrestha@gmail.com>
2022-08-11 12:03:46 +0545

    Updates

---
+++ precache-manifest.336665b3ecf6423472179ed7814f2618.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f5c7b2cd8bf0ac92683",
+    "url": "/textutilsagain/static/css/main.1136a80b.chunk.css"
+  },
+  {
+    "revision": "5f5c7b2cd8bf0ac92683",
+    "url": "/textutilsagain/static/js/main.fc9c3a10.chunk.js"
+  },
+  {
+    "revision": "5ae511726896f9724a32",
+    "url": "/textutilsagain/static/js/runtime~main.759e50bd.js"
+  },
+  {
+    "revision": "82f01422aae9b8a8aad8",
+    "url": "/textutilsagain/static/js/2.3e5a4b81.chunk.js"
+  },
+  {
+    "revision": "a9807912173ef280ea3f",
+    "url": "/textutilsagain/static/js/3.9f01db21.chunk.js"
+  },
+  {
+    "revision": "6ce494d335d662cfe7dc51a1b6d584ff",
+    "url": "/textutilsagain/static/media/logo.6ce494d3.png"
+  },
+  {
+    "revision": "5749529ba1575c1975c2e47142adb6bc",
+    "url": "/textutilsagain/index.html"
+  }
+];
